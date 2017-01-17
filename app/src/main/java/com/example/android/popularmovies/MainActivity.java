e26656c295691692b17198896f64f8f82742324e
virginiar <virginia.redondoanton@gmail.com>
2017-01-17 14:02:54 +0100

    Add HTTP query and AsyncTask to get JSON movies data

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -7,9 +8,12 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mtextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,31 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = (ImageView) findViewById(R.id.image_view);
         Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into(imageView);
 
-        TextView textView = (TextView) findViewById(R.id.text_view);
+        mtextView = (TextView) findViewById(R.id.text_view);
         URL queryUrl = QueryUtils.buildUrl(QueryUtils.POPULAR_QUERY);
-        textView.setText(queryUrl.toString());
+        //mtextView.setText(queryUrl.toString());
+        new MoviesQueryTask().execute(queryUrl);
+    }
+
+    public class MoviesQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL queryURL = urls[0];
+            String moviesResult = null;
+            try {
+                moviesResult = QueryUtils.getResponseFromHttpUrl(queryURL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return moviesResult;
+        }
+
+        @Override
+        protected void onPostExecute(String moviesResult) {
+            if (moviesResult != null && !moviesResult.equals("")) {
+                mtextView.setText(moviesResult);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/android/popularmovies/QueryUtils.java
+++ app/src/main/java/com/example/android/popularmovies/QueryUtils.java
@@ -1,10 +1,13 @@
 package com.example.android.popularmovies;
 
 import android.net.Uri;
-import android.util.Log;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public final class QueryUtils {
 
@@ -39,8 +42,32 @@ public final class QueryUtils {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
-        Log.d(LOG_TAG, "Built URI " + url);
         return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 }
